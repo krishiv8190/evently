a68d16b6be73c1717dde70a3b9c4d7585afee7aa
Krishiv <avin.pandita32@gmail.com>
2024-03-01 21:21:22 +0530

    fix merge conflicts

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import React from 'react'
-<<<<<<< HEAD
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
@@ -45,45 +44,6 @@ const EventForm = ({ userId, type }: EventFormProps) => {
 
   return (
     <Form {...form}>
-=======
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
-import { Button } from "@/components/ui/button"
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage,} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
- 
-const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
-})
-
-
-type EventFormProps = {
-    userId: string
-    type: "Create" | "Update"
-}
-
-const EventForm = ({userId,type}: EventFormProps) => {
-
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
-        defaultValues: {
-          username: "",
-        },
-      })
-     
-      // 2. Define a submit handler.
-      function onSubmit(values: z.infer<typeof formSchema>) {
-        // Do something with the form values.
-        // ✅ This will be type-safe and validated.
-        console.log(values)
-      }
-
-  return (
-<Form {...form}>
->>>>>>> 36eb5574c02b8c42303879ebba18ff3b21463027
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
         <FormField
           control={form.control}
@@ -103,15 +63,8 @@ const EventForm = ({userId,type}: EventFormProps) => {
         />
         <Button type="submit">Submit</Button>
       </form>
-<<<<<<< HEAD
     </Form>
   )
 }
 
 export default EventForm
-=======
-    </Form>  )
-}
-
-export default EventForm
->>>>>>> 36eb5574c02b8c42303879ebba18ff3b21463027
