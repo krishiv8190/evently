f550f99c2e1ffba4147bea5cdf543dc40471b7c3
Krishiv <avin.pandita32@gmail.com>
2024-03-02 23:31:21 +0530

    Implement Form Submission and Control

--- middleware.ts
+++ middleware.ts
@@ -3,12 +3,14 @@ import { authMiddleware } from '@clerk/nextjs'
 export default authMiddleware({
   publicRoutes: [
     '/',
+    '/api/webhooks',
     '/events/:id',
     '/api/webhook/clerk',
     '/api/webhook/stripe',
     '/api/uploadthing',
   ],
   ignoredRoutes: [
+    '/api/webhooks',
     '/api/webhook/clerk',
     '/api/webhook/stripe',
     '/api/uploadthing',
