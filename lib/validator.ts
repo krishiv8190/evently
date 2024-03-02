e6bb66d09bdf29c00eec869dc726cc1e7eb6b2d7
Krishiv <avin.pandita32@gmail.com>
2024-03-02 15:05:41 +0530

    Complete Form Implementation

--- lib/validator.ts
+++ lib/validator.ts
@@ -1,19 +1,19 @@
-import { z } from 'zod'
+import * as z from 'zod'
 
 export const eventFormSchema = z.object({
-  title: z.string().min(3, 'Title must be at least 3 characters long'),
+  title: z.string().min(3, 'Title must be at least 3 characters'),
   description: z
     .string()
-    .min(10, 'Description must be at least 10 characters long')
-    .max(100, 'Description must be at most 100 characters long'),
+    .min(3, 'Description must be at least 3 characters')
+    .max(400, 'Description must be less than 400 characters'),
   location: z
     .string()
-    .min(3, 'Location must be at least 3 characters long')
-    .max(400, 'Location must be at most 400 characters long'),
+    .min(3, 'Location must be at least 3 characters')
+    .max(400, 'Location must be less than 400 characters'),
   imageUrl: z.string(),
   startDateTime: z.date(),
   endDateTime: z.date(),
-  category: z.string(),
+  categoryId: z.string(),
   price: z.string(),
   isFree: z.boolean(),
   url: z.string().url(),
