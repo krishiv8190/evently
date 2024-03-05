91bf924c301fb4709d8a026458a59f2d99c2a434
Krishiv <avin.pandita32@gmail.com>
2024-03-05 17:00:48 +0530

    Implement Update/Delete functions

--- app/(root)/events/[id]/update/page.tsx
+++ app/(root)/events/[id]/update/page.tsx
@@ -1,8 +1,17 @@
 import EventForm from '@/components/shared/EventForm'
+import { getEventById } from '@/lib/actions/event.actions'
+import { UpdateEventParams } from '@/types'
 import { auth } from '@clerk/nextjs'
 import React from 'react'
 
-const UpdateEvent = () => {
+type UpdateEventProps = {
+  params: {
+    id: string
+  }
+}
+
+const UpdateEvent = async ({ params: { id } }: UpdateEventProps) => {
+  const event = await getEventById(id)
   const { sessionClaims } = auth()
 
   const userId = sessionClaims?.userId as string
@@ -14,7 +23,12 @@ const UpdateEvent = () => {
         </h3>
       </section>
       <div className="wrapper my-8">
-        <EventForm userId={userId} type="Update" />
+        <EventForm
+          userId={userId}
+          eventId={event._id}
+          type="Update"
+          event={event}
+        />
       </div>
     </>
   )
