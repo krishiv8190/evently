5b7206dd3d25f02a398753b90149053f59ec32f2
Krishiv <avin.pandita32@gmail.com>
2024-03-06 15:46:28 +0530

    Fix some issues

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -8,47 +8,51 @@ import { connectToDatabase } from '../database'
 import Order from '../database/models/order.model'
 
 export const checkoutOrder = async (order: CheckoutOrderParams) => {
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+
+  const price = order.isFree ? 0 : Number(order.price) * 100;
+
   try {
-    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!)
-    const price = order.isFree ? 0 : Number(order.price) * 100
-    // Create Checkout Sessions from body params.
     const session = await stripe.checkout.sessions.create({
       line_items: [
         {
           price_data: {
-            currency: 'inr',
+            currency: 'usd',
             unit_amount: price,
             product_data: {
-              name: order.eventTitle,
-            },
+              name: order.eventTitle
+            }
           },
-          quantity: 1,
+          quantity: 1
         },
       ],
-      metadata: { eventId: order.eventId, buyerId: order.buyerId },
-
+      metadata: {
+        eventId: order.eventId,
+        buyerId: order.buyerId,
+      },
       mode: 'payment',
-      success_url: `${process.env.NEXT_PUBLIC_DOMAIN}/profile`,
-      cancel_url: `${process.env.NEXT_PUBLIC_DOMAIN}/`,
-    })
+      success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/profile`,
+      cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/`,
+    });
 
     redirect(session.url!)
   } catch (error) {
-    throw error
+    throw error;
   }
 }
 
 export const createOrder = async (order: CreateOrderParams) => {
   try {
-    await connectToDatabase()
+    await connectToDatabase();
+    
     const newOrder = await Order.create({
       ...order,
       event: order.eventId,
       buyer: order.buyerId,
-    })
+    });
 
-    return JSON.parse(JSON.stringify(newOrder))
+    return JSON.parse(JSON.stringify(newOrder));
   } catch (error) {
-    handleError(error)
+    handleError(error);
   }
 }
