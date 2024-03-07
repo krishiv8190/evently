58c6c46f6227f1a8fd5bad23fe8d8398da561fe0
Krishiv <avin.pandita32@gmail.com>
2024-03-07 12:27:43 +0530

    Implement search functionality

--- app/(root)/orders/page.tsx
+++ app/(root)/orders/page.tsx
@@ -1,3 +1,4 @@
+import Search from '@/components/shared/Search'
 import { getOrdersByEvent } from '@/lib/actions/order.actions'
 import { formatDateTime, formatPrice } from '@/lib/utils'
 import { SearchParamProps } from '@/types'
@@ -15,7 +16,9 @@ const Orders = async ({ searchParams }: SearchParamProps) => {
         <h3 className="wrapper h3-bold text-center sm:text-left ">Orders</h3>
       </section>
 
-      <section className="wrapper mt-8"></section>
+      <section className="wrapper mt-8">
+        <Search placeholder="Search buyer name..." />
+      </section>
 
       <section className="wrapper overflow-x-auto">
         <table className="w-full border-collapse border-t">
